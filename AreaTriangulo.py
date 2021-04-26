113b68a33e6bb3edee20587b2bc0bcd8d6154bbf
LuisNina <>
2021-04-26 19:41:16 +0000

    trabajo

---
+++ AreaTriangulo.py
@@ -0,0 +1,9 @@
+#Variables de entrada y otros 
+print("Ejercicio 01: Area Triangulo")
+#Datos de entrada ingresados por dispositivos de entrada
+b=int(input("Ingrese la Base:"))
+h=int(input("Ingrese altura:"))
+#Proceso
+area=(b*h)/2
+#Datos de salida
+print("El area del Triangulo es:",area)
